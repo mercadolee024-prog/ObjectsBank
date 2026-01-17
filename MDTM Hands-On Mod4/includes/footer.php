03c8ab4c0983f68df52748fb31e8352659c0640a
mercadolee024-prog <mercadolee024@gmail.com>
2026-01-17 23:08:04 +0800

    Update footer.php

--- MDTM Hands-On Mod4/includes/footer.php
+++ MDTM Hands-On Mod4/includes/footer.php
@@ -1,8 +1,5 @@
   </body>
 </html>
-    <!-- ...your page content above... -->
-
-    <!-- Footer should be inside the body -->
     <footer>
         <p>&copy; <?= date('Y'); ?> Ritz & Riches. All rights reserved.</p>
     </footer>
